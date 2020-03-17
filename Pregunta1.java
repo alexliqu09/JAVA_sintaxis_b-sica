c2ddf51ff97f291feaf3e894f83d13efdd6d5ca9
Alexander Leonardo Lique Lamas <57840850+alexliqu09@users.noreply.github.com>
2020-03-17 15:28:43 -0500

    Add files via upload

---
+++ Pregunta1.java
@@ -0,0 +1,41 @@
+
+
+package pregunta1;
+
+import java.util.Scanner;
+
+
+public class Pregunta1 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada=new Scanner(System.in);
+        int opcion = 0;
+        do{
+            System.out.print("Seleccione un valor");
+            opcion=entrada.nextInt();
+        switch(opcion){
+            case 1:break;
+            case 2: pregunta2(); entrada.next();System.out.print("save");break;
+            case 3: pregunta3();entrada.next();break;
+            case 4:pregunta4();entrada.next();break;
+        }
+        }while(opcion!=1);
+        
+        }
+      public static void pregunta2(){
+          System.out.print("Sexo:");
+        
+      }
+      public static void pregunta3(){
+          System.out.print("nombre: ");
+          
+      }
+      public static void pregunta4(){
+          System.out.print("edad: ");
+      }
+    }
+
+
+    
+
